cf6b694c1b6537d79954a65b923686f38b3ccb25
Ademar Tellecher <isotrinux@gmail.com>
2026-09-08 22:15:06 -0300

    chore: bump version to v2.2.1

--- frontend/app.js
+++ frontend/app.js
@@ -383,7 +383,7 @@ navEdit.addEventListener('click', () => {
 });
 navRemove.addEventListener('click', handleRemove);
 navAbout.addEventListener('click', () => {
-    showToast('IP Monitor v2.2 • Wails v3 + SQLite3');
+    showToast('IP Monitor v2.2.1 • Wails v3 + SQLite3');
 });
 
 // Listener para eventos periódicos emitidos pelo backend Go (Wails v3)
